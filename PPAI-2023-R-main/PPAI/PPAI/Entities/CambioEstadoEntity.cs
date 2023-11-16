669415f4a877ac9b4ef69e9161013d296eecb58a
Harumi236 <harumi236@proton.me>
2023-11-16 08:10:08 -0300

    Limpieza de imports, creacion de un constructor y nuevo Field para fechaFin

--- PPAI-2023-R-main/PPAI/PPAI/Entities/CambioEstadoEntity.cs
+++ PPAI-2023-R-main/PPAI/PPAI/Entities/CambioEstadoEntity.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PPAI.Entities {
     public class CambioEstadoEntity {
+        // Fields
         private DateTime fechaHoraInicio;
+        private DateTime fechaHoraFin;
         private EstadoEntity estado = new EstadoEntity();
         private int id;
 
+        // Constructor
+        public CambioEstadoEntity(DateTime fechaHoraInicio, EstadoEntity estado)
+        {
+            this.fechaHoraInicio = fechaHoraInicio;
+            this.estado = estado;
+        }
+        
+        // Getters & Setters
         public DateTime FechaHoraInicio { get => fechaHoraInicio; set => fechaHoraInicio = value; }
+        public DateTime FechaHoraFin { get => fechaHoraFin; set => fechaHoraFin = value; }
         public EstadoEntity Estado { get => estado; set => estado = value; }
         public int Id { get => id; set => id = value; }
     }
