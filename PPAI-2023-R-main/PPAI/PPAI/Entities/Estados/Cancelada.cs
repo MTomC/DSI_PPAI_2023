7e3c15ac60817da60457665e6cc317a748012206
Harumi236 <harumi236@proton.me>
2023-11-27 17:48:34 -0300

    Newest version

---
+++ PPAI-2023-R-main/PPAI/PPAI/Entities/Estados/Cancelada.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI.Entities.Estado
+{
+    public class Cancelada : EstadoA
+    {
+        public Cancelada()
+        {
+            Id = 4;
+            Nombre = "Cancelada";
+        }
+        public override EstadoA CrearProximoEstado(LlamadaEntity llamada)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool EsCancelada()
+        {
+            return true;
+        }
+    }
+}
